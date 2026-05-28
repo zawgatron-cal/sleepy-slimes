330a436f7c554220bf3b19662afbabed1603cc7d
Winston Luo <winstonluo07@berkeley.edu>
2026-05-28 14:10:50 -0700

    random experimenting stuff

--- app/renderer-test.tsx
+++ app/renderer-test.tsx
@@ -7,9 +7,12 @@ import { useRouter } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { mainScreens } from '@/src/theme/mainScreensTheme';
 import { createAppStyles } from '@/src/theme/createAppStyles';
-import { useRef, useState } from 'react';
-import { Canvas, useFrame } from '@react-three/fiber/native';
-import { Object3D, Sphere, SphereGeometry } from 'three';
+import { Suspense, useRef, useState } from 'react';
+import { Canvas, useFrame} from '@react-three/fiber/native';
+import { MeshDistortMaterial, MeshTransmissionMaterial, useTexture } from "@react-three/drei/native";
+import { Material, MeshToonMaterial, Object3D, RepeatWrapping, Sphere, SphereGeometry, TextureLoader } from 'three';
+import useControls from 'r3f-native-orbitcontrols';
+
 
 
 function cycleColor(color: string) {
@@ -30,7 +33,7 @@ function Box(props) {
     {
       setSpeed(1);
     }
-    meshRef.current.rotation.z += delta * speed;
+    // meshRef.current.rotation.z += delta * speed;
     meshRef.current.rotation.x += delta * speed;
   });
   return (
@@ -44,10 +47,49 @@ function Box(props) {
     >
       <boxGeometry args={[1, 1, 1]} />
       {/* <sphereGeometry args={[1, 32, 32]} /> */}
-      <meshStandardMaterial color={color} roughness={0}/>
+      <meshToonMaterial color={color}/>
     </mesh>
   );
 }
+
+function Slime(props) {
+
+  const colorMap = useTexture(require('@/assets/epic-slime-texture.png'));
+
+  return (
+    <mesh {...props}>
+      <sphereGeometry args={[1, 128, 128]} />
+      {/* <meshStandardMaterial>
+        <color args={[1, 0.3, 1]} attach="color" />
+      </meshStandardMaterial> */}
+      <meshToonMaterial map = {colorMap} roughness={0}/>
+    </mesh>
+  );
+}
+function BrickSphere(props) {
+  const maps = useTexture(
+    {
+      map: require('@/assets/PavingStones092_1K-PNG/PavingStones092_1K-PNG_Color.png'),
+      displacementMap: require('@/assets/PavingStones092_1K-PNG/PavingStones092_1K-PNG_Displacement.png'),
+      normalMap: require('@/assets/PavingStones092_1K-PNG/PavingStones092_1K-PNG_NormalGL.png'),
+      roughnessMap: require('@/assets/PavingStones092_1K-PNG/PavingStones092_1K-PNG_Roughness.png'),
+      aoMap: require('@/assets/PavingStones092_1K-PNG/PavingStones092_1K-PNG_AmbientOcclusion.png'),
+    },
+    (textures) => {
+      Object.values(textures).forEach((tex) => {
+        tex.wrapS = tex.wrapT = RepeatWrapping;
+        tex.repeat.set(1, 1);
+      });
+    },
+  );
+  
+  return (
+    <mesh {...props}>
+      <sphereGeometry args={[1, 64, 64]} />
+      <meshStandardMaterial {...maps} normalScale = {3} displacementScale={0.2} />
+    </mesh>
+  );
+}  
 
 function RotatingSpotLight(props) {
   const lightRef = useRef(null);
@@ -67,21 +109,30 @@ function RotatingSpotLight(props) {
 export default function RendererTestScreen() {
   const router = useRouter();
 
+  const [OrbitControls, events] = useControls();
   return (
-    <Canvas>
-      {/* <ambientLight intensity={Math.PI / 2} />
-      <pointLight position = {[0, 0, 0]} intensity={10}></pointLight>
-      <mesh>
-        <sphereGeometry />
-        <meshStandardMaterial color="hotpink" />
-      </mesh> */}
+    <SafeAreaView style={styles.container} {...events}>
+      <Canvas>
+        <OrbitControls enableRotate={true} enableZoom={false} enablePan={false}/>
+        <Suspense fallback={null}>
+          {/* <ambientLight intensity={Math.PI / 2} />
+          <pointLight position = {[0, 0, 0]} intensity={10}></pointLight>
+          <mesh>
+            <sphereGeometry />
+            <meshStandardMaterial color="hotpink" />
+          </mesh> */}
 
-      <ambientLight intensity={Math.PI / 3} />
-      <RotatingSpotLight position={[0, 0, 0]}/>
-      {/* <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} /> */}
-      <Box position={[0, 0, 0]} />
-    </Canvas>
+          <ambientLight color = {[1, 1, 1]}intensity={0.5 * Math.PI} />
+          <spotLight position={[0, 10, 0]} color = "white" angle={Math.PI / 3} penumbra={1} decay={1} intensity={5 * Math.PI}/>  
+          <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+          {/* <Box position={[0, 0, 0]} /> */}
+          <Slime position={[0, 0, 0]}/>
+          {/* <BrickSphere position={[0, 0, 0]} /> */}
+        </Suspense>
+      </Canvas>
+    </SafeAreaView>
   );
+  
 }
 
 const styles = createAppStyles({
